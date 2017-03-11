3a9b4d956141aaa0a59c6b46e3a678256caf9282
nangarehrushikesh91 <nangarehrushikesh91@gmail.com>
2017-03-11 18:51:00 +0530

    Submission of assignment
    Submission of assignment

---
+++ app.js
@@ -0,0 +1,17 @@
+var total = 0;
+for(var i = 1; i <= 1000; i++){
+total += i;
+}
+console.log("Using for loop : "+total);
+	
+var N = 1000;
+var sum = 0;
+var j=1;
+while(j<=N) {
+		sum+=j;
+		j++;
+}   
+console.log("Using while loop : " + sum);
+    
+  
+
